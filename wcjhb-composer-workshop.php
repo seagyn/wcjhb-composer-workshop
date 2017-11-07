be1c2e564b2972782c9d3a4d839121ca95ad8be6
Seagyn <seagyn@digitalleap.co.za>
2017-11-07 21:49:55 +0200

    Initial plugin

---
+++ wcjhb-composer-workshop.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name:  WordCamp Joburg Composer Workshop
+ * Plugin URI:   https://www.seagyndavis.com/wcjhb2017/
+ * Description:  Just a basic plugin to get us going.
+ * Version:      1.0
+ * Author:       Seagyn Davis
+ * Author URI:   https://www.seagyndavis.com/
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  wcjhb-composer-workshop
+ */
